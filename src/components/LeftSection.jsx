cf7244445466c204c302b5a9d2045d4f9cf114af
Rinku Hasija <rishabhasija5454@gmail.com>
2023-02-15 23:35:04 +0530

    initial commit

---
+++ src/components/LeftSection.jsx
@@ -0,0 +1,20 @@
+import './LeftSection.css'
+
+function LeftSection() {
+    return (
+
+        <div className="left col-md-6">
+            <h1> Learn to code by watching others </h1>
+            <br />
+
+            <p>
+                See how experienced developers solve problems in real-time.
+                Watching scripted tutorials is great, but understanding how
+                developers think is invaluable.
+            </p>
+
+        </div>
+    )
+}
+
+export default LeftSection
---
+++ src/components/RightSection.jsx
@@ -0,0 +1,103 @@
+import './RightSection.css'
+import { useState, useEffect } from "react";
+import Select from 'react-select'
+
+const options = [
+    { value: 'HTML', label: 'HTML' },
+    { value: 'CSS', label: 'CSS' },
+    { value: 'JS', label: 'JS' },
+    { value:"NODE",label:"NODE"},
+    { value: "REACT", label: "REACT"}
+  ]
+
+
+
+
+function RightSection() {
+
+    const [formValues, setFromValues] = useState({"name":"","email":"","password":""})
+    const [select, setSelect] = useState([])
+    const [selectPlaceHolder, setSelectPlaceHolder] = useState("Choose Skills")
+    const [active, setActive] = useState(false)
+    const [header,setHeader]= useState("Try it free 7 days then ₹180/mo. thereafter")
+
+    function handleChange(e) {
+        setFromValues({...formValues,[e.target.name]:e.target.value})       
+    }
+
+    function handleSelect(e) {
+        setSelect((prev)=>[...prev,{...e[e.length-1]}])
+    }
+
+    const claimTrial= (e)=>{
+        e.preventDefault();
+        setHeader("You have successfully subscribed to our plan ✓")
+        setFromValues({"name":"","email":"","password":""})
+        setSelect([])
+        setActive(false)
+        setSelectPlaceHolder((prev)=>prev)
+        setTimeout(() => {
+            setHeader("Try it free 7 days then ₹180/mo. thereafter");
+        }, 5000);
+    }
+
+    const isFromSubmit =()=>{
+        for (const value in formValues) {
+            if(formValues[value].length==0){
+                return false
+            }
+        }
+        return true
+    }
+
+    useEffect(()=>{
+        if(select.length>0 && isFromSubmit()){
+            setActive(true)
+        }
+    },[select,formValues])
+
+    return (
+        <div className="right col-md-6">
+
+            <div className="alert">
+
+                <p> {header} </p>
+
+            </div>
+
+            <div className='form'>
+
+                <div className="inside-form">
+
+                    <form>
+
+                        <input className="input first" name='name' onChange={handleChange} type="text" placeholder="Name" value={formValues.name} /><br /><br />
+
+                        <input className="input" onChange={handleChange} placeholder="Email Address" type="email" id="email" name="email" value={formValues.email} /><br /><br />
+
+                        <input className="input" onChange={handleChange} placeholder=" Password" type="password" name='password' value={formValues.password} /><br /><br />
+
+                        <Select className="select" options={options} placeholder={selectPlaceHolder} onChange={handleSelect} isMulti isClearable={true} hideSelectedOptions={true} id="skills" name="skills" /><br /><br />
+
+                        {active?<button onClick={claimTrial} className="input last">CLAIM YOUR FREE TRAIL</button>: <button onClick={claimTrial} className="input form-btn">CLAIM YOUR FREE TRIAL</button>}
+
+                        <div className="last_div">
+                            <p> By clicking the button you are agreeing to our  <span> <strong>   Terms and Services  </strong></span>  </p>
+                        </div>
+
+
+                    </form>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    )
+
+
+
+}
+
+export default RightSection
